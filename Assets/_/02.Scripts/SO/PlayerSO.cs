eeb456afec9a120f6ae8f9aeffa0c0afb69e3255
KHJ_COM2 <ggm.s21002@ggh.goe.go.kr>
2023-03-11 19:32:45 +0900

    2023.03.11 파일정리

---
+++ Assets/_/02.Scripts/SO/PlayerSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player", menuName = "PlayerMovementSystem/Characters/Player")]
+public class PlayerSO : ScriptableObject
+{
+    [field: SerializeField] public PlayerGroundedData GroundedData { get; private set; }
+    [field: SerializeField] public PlayerAirborneData AirborneData { get; private set; }
+}
